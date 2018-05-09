dba5e7c182ac6db7a55fce557a9ffb6d2f96cf8f
David Ormsbee <dave@edx.org>
2018-05-09 09:07:54 -0400

    Move update_discussions_map to django_comment_common
    The task to update the mapping of inline discussion XBlocks to
    discussion_ids was originally implemented in an LMS-only process, and
    did not properly catch a course_published signal from Studio. I tried
    to enable the Discussions app in Studio (it's a pluggable Django app),
    but this resulted in difficult to debug bok choy errors around cohorts.
    
    This commit moves the task into django_comment_common, which is also
    where the model it's altering is located. It is possible to just move
    the listener to django_comment_common and keep the task in LMS and
    bridge the two with routing, but I thought that would be unnecessarily
    confusing.

--- lms/djangoapps/discussion/tasks.py
+++ lms/djangoapps/discussion/tasks.py
@@ -33,24 +33,6 @@ DEFAULT_LANGUAGE = 'en'
 ROUTING_KEY = getattr(settings, 'ACE_ROUTING_KEY', None)
 
 
-@task(base=LoggedTask)
-def update_discussions_map(context):
-    """
-    Updates the mapping between discussion_id to discussion block usage key
-    for all discussion blocks in the given course.
-
-    context is a dict that contains:
-        course_id (string): identifier of the course
-    """
-    course_key = CourseKey.from_string(context['course_id'])
-    discussion_blocks = get_accessible_discussion_xblocks_by_course_id(course_key, include_all=True)
-    discussions_id_map = {
-        discussion_block.discussion_id: unicode(discussion_block.location)
-        for discussion_block in discussion_blocks
-    }
-    set_course_discussion_settings(course_key, discussions_id_map=discussions_id_map)
-
-
 class ResponseNotification(MessageType):
     pass
 
